e7e45ef7c031a7ed13bfa9d9183520b7ab413204
ivanhoe1982 <ivanhoe1982@gmail.com>
2015-03-08 00:42:40 +0100

    functions calculated top-down with binding of this

--- doodle.js
+++ doodle.js
@@ -1,100 +1,47 @@
 'use strict';
 
-//helper functions
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-var ARGUMENT_NAMES = /([^\s,]+)/g;
-
-function getParamNames(func) {
-    var fnStr = func.toString().replace(STRIP_COMMENTS, '');
-    var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-    if(result === null)
-        result = [];
-    return result;
-}
-//end helper functions
-
-
 //PART 1
 //declaration of the business context, i.e. an object containing all data from ODS-P and RDSPLUS required for this case
-//this can me mightly nested (and probably will contain nested objects as well as arrays) but nesting is currently not supported
-//this is what the LoaderService will return after making all the calls
+//this can me mightly nested (and probably will contain nested objects as well as arrays)
+//this is the object the LoaderService will return after making all the calls, and that we can show (parts of) in the application
 var businessContext = {
-    rf: 0.03,
+    rf: 2,
     beta:1.5,
-    rm:0.05
+    rm: 5,
+    stuff : {
+        a: 1,
+        b: 2,
+        series:[2,4,6,8,10] //=30
+    }
 };
 
 
 //PART 2
 //declaration of functions representing individual formulas
-function costOfCapital(rf,beta,rm) {
-    return rf + beta*(rm-rf);
+//this example starts from the TOP and we progressively "deconstruct" the businessContext into smaller pieces
+//what's important, is that we do not specifically declare parameters in functions, but rather pass it with "call" and
+//bind to "this" of each function - this allows us to quickly prototype a complex tree and pass large structures
+//without gigantic function declarations
+
+function functionA() {
+    return functionB1.call(this.stuff)+functionB2.call(this);
 }
 
-function costOfCapital2(rf,beta,rm) {
-    return rf + beta*(rm-rf) +1;
+function functionB1() {
+    return this.a*this.b*functionC1.call(this.series); //1*2*30 = 60
 }
 
-function costOfCapital3(rf,beta,rm) {
-    return rf + beta*(rm-rf) +2;
+function functionB2() {
+    return this.rf + this.beta*(this.rm-this.rf); //2 + 1.5*(5-2) = 6.5
 }
 
-//we have to declare a list of all TOP LEVEL formulas that we want to execute later on
-//if there is only one top level formula than this will be of little use
-var formulaSystem  = [costOfCapital,costOfCapital2,costOfCapital3];
-
-var injector = function(f){
-    var lookup = function() {
-        console.log('injection');
-        f.apply(null,arguments);
-    };
-
-    return lookup;
-};
-
-
-//PART 3
-//loop through all atomic formulas, lookup arguments they need in businessContext and execute them
-//then store the result in the result object that can be consumed by the visualization
-
-//this 'matching' of arguments of functions from businessContext is what allows us for short, reusable code
-//using for loops instead of maps for sake of clarity of code
-
-function executeAll(formulaSystem,businessContext){
-    var result = {}; //empty object
-    for (var i = 0; i<formulaSystem.length;  i++) {
-        //figure out names of parameters of any given function
-        var requiredParamNames=getParamNames(formulaSystem[i]);
-
-        var requiredParamValues=[];
-        //...and then look up values of the same name in businessContext
-        for(var j=0; j<requiredParamNames.length; j++) {
-            requiredParamValues[j]=businessContext[requiredParamNames[j]];
-        }
-        result[formulaSystem[i].name] = formulaSystem[i].apply(null,requiredParamValues);
-    }
-    return result;
+function functionC1(){ //basically sum up
+    return this.reduce(function(pv, cv) { return pv + cv; }, 0);
 }
 
-function executeAll2(formulaSystem,businessContext){
-    var result = {}; //empty object
-    for (var i = 0; i<formulaSystem.length;  i++) {
-        formulaSystem[i]=formulaSystem[i].bind(businessContext);
-
-        result[formulaSystem[i].name] = formulaSystem[i].apply(null,requiredParamValues);
-    }
-    return result;
-}
-
-var totalResults=executeAll(formulaSystem,businessContext);
-
-var totalResults2=executeAll2(formulaSystem,businessContext);
+var totalResults=functionA.call(businessContext); //66.5
 
 console.log(totalResults);
-console.log(totalResults2);
-
-//////2
-
 
 
 
